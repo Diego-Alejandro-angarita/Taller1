0a72abe8725fadb5b939e6e3f51c556d77a6e14a
Diego-Alejandro-angarita <daangarita@eafit.edu.co>
2026-02-05 12:05:40 -0500

    MVP point reached

---
+++ movie/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Movie(models.Model):
+    tittle = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='images/', blank=True)
+    url = models.URLField(blank=True)
